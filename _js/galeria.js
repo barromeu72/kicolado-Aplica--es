e811b94bd41df6d5e681d7e18e063d5406a7dbb1
barromeu72 <barromeu.git@gmail.com>
2020-10-11 19:17:32 -0300

    Criando o site kicolado aplicações

---
+++ _js/galeria.js
@@ -0,0 +1,20 @@
+let imagens = document.querySelectorAll('.small-img');
+let modal = document.querySelector('.modal');
+let modalImg = document.querySelector('#modal-img');
+let btnClose = document.querySelector('#btn-close');
+let srcVal = "";
+
+for(let i = 0; i < imagens.length; i++){
+    imagens[i].addEventListener('click', function(){
+
+        srcVal = imagens[i].getAttribute('src');
+
+        modalImg.setAttribute('src', srcVal);
+
+        modal.classList.toggle('modal-active');
+    });
+}
+
+btnClose.addEventListener('click', function(){
+    modal.classList.toggle('modal-active');
+});
---
+++ _js/script.js
@@ -0,0 +1,10 @@
+/*$('#texto').ripples({
+    dropRadius: 10,
+    pertubance: 0.03
+});*/
+$(document).ready(function(){
+$('#texto').ripples({
+    dropRadius: 12,
+    pertubance: 0.05
+});
+})
